7adfd22c43b6560c750279394cc85834e72e766b
Nate Olson <nolson@nist.gov>
2018-03-28 14:49:58 -0400

    initial commit

---
+++ R/metagenomeSE.R
@@ -0,0 +1,14 @@
+###############################################
+#' metagenomeSE Package.
+#'
+#' Package defining metagenmeSE class and methods
+#'
+#' @name metagenomeSE-package
+#' @author Nathan D. Olson \email{nolson@@umiacs.umd.edu}
+#' @docType package
+#' @keywords package
+#' @seealso \link[metagenomeSE]{mgSE-class}
+#' @examples
+#' print("We have a package!")
+NA
+###############################################
