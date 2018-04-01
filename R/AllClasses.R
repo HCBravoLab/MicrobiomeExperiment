ec2db2fcc95e6117b7ea84b32e16934ee86d8d32
hcorrada <hcorrada@gmail.com>
2018-04-01 11:33:16 -0400

    update class documentation

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,9 +1,15 @@
 #' MicrobiomeExperiment-class
 #'
+#' SummarizedExperiment-like class for microbiome data. rowData is a
+#' MicrobiomeFeatures object LINK so it includes: a taxonomy table (DataFrame),
+#' optional phylogentic tree (phylo object), and a sequence database.
+#'
+#' It supports (most) of the interface to phyloseq objects
+#'
 #' @importClassesFrom metagenomeFeatures mgFeatures
 #' @importClassesFrom SummarizedExperiment SummarizedExperiment
 #'
-#' @example
+#' @examples
 #'
 #' library(metagenomeFeatures)
 #' data(mock_mgF)
@@ -12,7 +18,8 @@
 #' pd <- DataFrame(a=letters[1:4], b=1:4)
 #' numcounts <- nrow(mock_mgF) * 4
 #' counts <- matrix(sample(1:1000,numcounts,replace=TRUE), nr=nrow(mock_mgF), nc=4)
-#' MicrobiomeExperiment(assays=SimpleList(counts=counts)
+#'
+#' MicrobiomeExperiment(assays=SimpleList(counts=counts),
 #' rowData=mock_mgF,
 #' colData=pd
 #' )
