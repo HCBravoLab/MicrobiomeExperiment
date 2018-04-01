753bf78194c938152cbe391fb0b76445b078d292
LiNk-NY <mramos09@gmail.com>
2018-04-01 17:29:27 -0400

    add setAs template

--- R/MicrobiomeFeatures-class.R
+++ R/MicrobiomeFeatures-class.R
@@ -13,3 +13,7 @@ MicrobiomeFeatures <- function(taxa = DataFrame(), tree=NULL,
     mgF <- metagenomeFeatures::mgFeatures(taxa, tree, seq, metadata)
     .MicrobiomeFeatures(mgF)
 }
+
+setAs("DataFrame", "MicrobiomeFeatures", function(from) {
+    ## TODO: add coercion method for DataFrame to MicrobiomeFeatures
+})
