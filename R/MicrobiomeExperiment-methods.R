d5c7cde466fcffc2a7dc0502a92b82f3af6df541
Domenick Braccia <dbraccia@umd.edu>
2018-12-20 09:27:22 -0500

    updating DESCRIPTION, NAMESPACE and ME scripts

--- R/MicrobiomeExperiment-methods.R
+++ R/MicrobiomeExperiment-methods.R
@@ -1,3 +1,15 @@
 ## Add methods:
 # Implementing IHW (possibly as an agg.fun? not sure how it should be
 # implemented.. possibly run ihw() on output from metagenomeSeq::fitFeatureModel
+
+#' Method to aggregate __ using IHW
+#' @param p_values
+#' @param covar
+#' @param alpha
+# setMethod("aggIHW", "INPUT",
+#           function(
+#            p_values,
+#            covar,
+#            alpha = 0.1
+#           )
+#  )
